c6005d6d29e10d41ab7e6554898efc4c89b58664
Qi shao <qishking@163.com>
2024-11-15 22:20:29 +0800

    fix action

--- get_publicip.py
+++ get_publicip.py
@@ -1,4 +1,8 @@
-import urllib3
+import urllib
 
-ip_txt = urllib3.request(url='https://ipapi.co/ip/', method='get').data
+# ip_txt = urllib3.request(url='https://ipapi.co/ip/', method='get').data
+ip_txt = urllib.request.urlopen(url='https://ipapi.co/ip/').read()
+
 print(ip_txt)
+with open('public_ip.txt', 'w') as f:
+    f.write(str(ip_txt))
